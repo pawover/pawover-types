ab14880902bb7488c4dda701914c6deba68c29fb
pawover <pawover@outlook.com>
2026-06-29 14:49:35 +0800

    fix(global): 变更 PropsWithChildren 和 PropsWithRef 的属性为可选

--- src/react.ts
+++ src/react.ts
@@ -1,5 +1,5 @@
 import type { ReactNode, RefObject } from "react";
 
 export type Props<P = unknown> = Readonly<P>;
-export type PropsWithChildren<P = unknown> = Readonly<P> & { readonly children: ReactNode };
-export type PropsWithRef<P = unknown, R = unknown> = Readonly<P> & { readonly ref: RefObject<R> };
+export type PropsWithChildren<P = unknown> = Readonly<P> & { readonly children?: ReactNode | undefined };
+export type PropsWithRef<P = unknown, R = unknown> = Readonly<P> & { readonly ref?: RefObject<R> | undefined };
