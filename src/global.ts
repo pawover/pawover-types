5c1bc786e1dafb38834dd6d3fca56190dd746a25
pawover <pawover@outlook.com>
2026-07-01 14:44:45 +0800

    feat(global): 新增 Primitive/IsPrimitive/HasStringIndex 类型

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,5 @@
-/* eslint-disable ts/no-explicit-any */
-
+/** 原始类型 */
+export type Primitive = null | undefined | string | number | boolean | symbol | bigint;
 /** 任意对象类型 */
 export type AnyObject<K extends PropertyKey = PropertyKey, T = any> = Record<K, T>;
 /** 普通对象类型 */
@@ -16,3 +16,7 @@ export type AnyGeneratorFunction<P extends any[] = any[], T = any, R = any, N = 
 export type AnyAsyncGeneratorFunction<P extends any[] = any[], T = any, R = any, N = any> = (...args: P) => AsyncGenerator<T, R, N>;
 /** 描述无返回值函数 */
 export type VoidFunction = () => void;
+/** 判断是否为原始类型 */
+export type IsPrimitive<T> = T extends Primitive ? true : false;
+/** 判断是否具有索引签名 */
+export type HasStringIndex<T> = string extends keyof T ? true : false;
